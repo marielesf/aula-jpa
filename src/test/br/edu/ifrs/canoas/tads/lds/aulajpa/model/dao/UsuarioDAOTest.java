fb0d795610d2130f41441b1ec69f1054cf0ccf55
rodrigonoll <rodrigo.noll@canoas.ifrs.edu.br>
2015-03-04 23:59:21 -0300

    Comentado objeto que ainda não foi criado.

--- src/test/br/edu/ifrs/canoas/tads/lds/aulajpa/model/dao/UsuarioDAOTest.java
+++ src/test/br/edu/ifrs/canoas/tads/lds/aulajpa/model/dao/UsuarioDAOTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 
 public class UsuarioDAOTest {
 
-	UsuarioDAO usuarioDAO = new UsuarioDAO();
+	//UsuarioDAO usuarioDAO = new UsuarioDAO();
 
 	@Test
 	public void testSalvaNovoUsuario() {
